2b22e1356bc63f5d9b9e08b7fbe9eae820eb127c
mattchley <mattchley@gmail.com>
2019-12-10 12:54:38 -0800

    PostsNotesToSide

--- noteserver.js
+++ noteserver.js
@@ -3,7 +3,6 @@
 var express = require("express");
 var path = require("path");
 var fs = require('fs');
-let jsonData = require('./db/db.json')
 
 
 // Sets up the Express App
@@ -46,7 +45,7 @@ app.get("/api/notes", function (req, res) {
 // posts correctly but does not keep previous posts?
 app.post("/api/notes", function (req, res) {
   var note = req.body;
-  var notes = fs.readFile('./db/db.json', function (err) {
+  var notes = fs.readFile('./db/db.json', {'flags': 'a'},function (err) {
     if (err) throw err
     let parsedNotes;
     // If notes isn't an array or can't be turned into one, send back a new empty array
@@ -66,15 +65,47 @@ app.post("/api/notes", function (req, res) {
 
 // Delete Notes
 app.delete("/api/notes/:id", function (req, res) {
+// on the website it posts the title on the left with the name so best course of action is to delete by title
 
-  // //  accept id number
-  // var chosen = req.params.id;
-  // // read for id number
-  // const readID = fs.readFileSync('./db/db.json', { encoding: 'utf8' });
+  // var chosen = req.params.title;
+  // const readID = fs.readFile('./db/db.json', (err, data) => {
+  //   if (err) throw err;
+  //   res.json(read)
+  // });
+
+
+  // _id: req.params.user_id
+  //   }, function (err, user) {
+  //       if (err) return res.send(err);
+  //       res.json({ message: 'Deleted' });
+  //   });
   // // remove with specified id
   // return res.json()
   // // rewrite them to db.json
   // return res.json(false);
+// https://www.freecodecamp.org/news/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2/
+// example
+ 
+    // const id = req.params.id;
+    // const details = { '_id': new ObjectID(id) };
+    // db.collection('notes').remove(details, (err, item) => {
+    //   if (err) {
+    //     res.send({ 'error': 'An error has occurred' });
+    //   } else { res.send('Note ' + id + ' deleted!'); }
+    // });
+
+
+
+  //   delete(function(req, res) {
+  //     Bear.remove({
+  //         _id: req.params.bear_id
+  //     }, function(err, bear) {
+  //         if (err)
+  //             res.send(err);
+
+  //         res.json({ message: 'Successfully deleted' });
+  //     });
+  // });
 });
 
 
@@ -90,5 +121,5 @@ app.listen(PORT, function () {
 // connect the index.js functionality to the html
 // it appears that there arent some elements for the JS to connect to on the html
 
-// with ID templating on db.json
-[{ "title": "Test Title", "text": "Test text", "id": "1" }]
+// // with ID templating on db.json
+// [{ "title": "Test Title", "text": "Test text", "id": "1" }]
